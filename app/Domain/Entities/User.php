5e16eb4e680e7e567753a315bf9f9081a01c2cec
Pablito P Torrecampo Jr <83467428+pablitotorrecampojr@users.noreply.github.com>
2025-11-26 10:15:22 +0800

    torrexx: create seeder for [clean architecture]

--- app/Domain/Entities/User.php
+++ app/Domain/Entities/User.php
@@ -3,12 +3,26 @@ namespace App\Domain\Entities;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Database\Factories\UserFactory;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = [
+        'username', 
+        'first_name',
+        'last_name',
+        'email', 
+        'password'
+    ];
 
     protected $hidden = ['password', 'remember_token'];
+
+    public static function newFactory()
+    {
+        return UserFactory::new();
+    }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,12 +5,14 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Domain\Entities\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
     /**
      * The current password being used by the factory.
      */
@@ -24,8 +26,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'username'      => fake()->unique()->userName(),
+            'first_name'    => fake()->firstName(),
+            'last_name'     => fake()->lastName(),
+            'email'         => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
