ea86232ef51b9de66b6833141eb9d1c0d5e38321
Pablito P Torrecampo Jr <83467428+pablitotorrecampojr@users.noreply.github.com>
2025-11-29 22:40:33 +0800

    authentication: include mailer

--- app/Infrastructure/Repositories/EloquentUserOTPRepository.php
+++ app/Infrastructure/Repositories/EloquentUserOTPRepository.php
@@ -3,12 +3,15 @@ namespace App\Infrastructure\Repositories;
 use App\Models\UserOTP;
 use App\Models\User;
 use App\Domain\Repositories\IUserOTPRepository;
+use App\Mail\OTPMail;
+use Illuminate\Support\Facades\Mail;
 
 class EloquentUserOTPRepository implements IUserOTPRepository
 {
     public function send(User $user): void
     {
-        
+        $otp = rand(100000, 999999);
+        Mail::to($user->email)->send(new OTPMail($otp, $user->first_name));
     }
 
     public function verify(int $user_id, string $value): bool
