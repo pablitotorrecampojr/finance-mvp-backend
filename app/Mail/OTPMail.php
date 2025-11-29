745cc2128bd8f0b46b57bbd4822edca260707188
Pablito P Torrecampo Jr <83467428+pablitotorrecampojr@users.noreply.github.com>
2025-11-29 22:58:51 +0800

    authentication: enable sending of OTP

--- app/Mail/OTPMail.php
+++ app/Mail/OTPMail.php
@@ -39,7 +39,7 @@ class OTPMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'email.otp',
+            view: 'emails.otp',
             with: [
                 'otp' => $this->otp,
                 'first_name' => $this->firstName
