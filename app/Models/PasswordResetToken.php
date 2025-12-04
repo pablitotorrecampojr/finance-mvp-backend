645333cbd504cb27e458a407611e1ee6d762d540
Pablito P Torrecampo Jr <83467428+pablitotorrecampojr@users.noreply.github.com>
2025-12-04 15:10:34 +0800

    forgotpassword: create model for password tokens

---
+++ app/Models/PasswordResetToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordResetToken extends Model
+{
+    protected $table = 'password_reset_tokens';
+    public $timestamps = false;
+    public $fillable = [
+        'email',
+        'token',
+        'expired_at',
+        'created_at'
+    ];
+}
