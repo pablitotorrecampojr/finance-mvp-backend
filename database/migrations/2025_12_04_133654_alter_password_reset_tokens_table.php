92eb0dcb494c0d43367a208124ab0e19c574ff8f
Pablito P Torrecampo Jr <83467428+pablitotorrecampojr@users.noreply.github.com>
2025-12-04 21:40:22 +0800

    forogotpassword: alter password reset tokens

---
+++ database/migrations/2025_12_04_133654_alter_password_reset_tokens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('password_reset_tokens', function (Blueprint $table) {
+            $table->dropPrimary(['email']);
+        });
+
+        Schema::table('password_reset_tokens', function (Blueprint $table) {
+            $table->id()->first();
+            $table->string('email')->index()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $table->dropColumn('id');
+        $table->string('email')->unique()->change();
+    }
+};
+
+
