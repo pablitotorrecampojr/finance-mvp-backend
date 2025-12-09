b4ce6dd70c2a6bba09fc1cc795b1861a53e536ce
Pablito P Torrecampo Jr <83467428+pablitotorrecampojr@users.noreply.github.com>
2025-12-09 17:32:03 +0800

    expense: unique rules

--- app/Http/Requests/ExpenseCategoryRequest.php
+++ app/Http/Requests/ExpenseCategoryRequest.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ExpenseCategoryRequest extends FormRequest
 {
@@ -13,7 +14,13 @@ class ExpenseCategoryRequest extends FormRequest
     {
         return [
             'user_id' => 'required|integer',
-            'category' => 'required|string|min:6',
+            'category'   => [
+                'required',
+                'string',
+                'min:6',
+                Rule::unique('expense_categories', 'category')
+                    ->where(fn ($query) => $query->where('user_id', $this->user_id)),
+            ],
             'limit' => 'required|numeric',
             'limit_type' => 'required|string|in:daily,weekly,monthly,yearly',
         ];
