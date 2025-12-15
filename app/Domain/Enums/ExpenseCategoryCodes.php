196a7f0531dcbb6905377a1d260ca8dc32037c97
Pablito P Torrecampo Jr <83467428+pablitotorrecampojr@users.noreply.github.com>
2025-12-15 17:55:17 +0800

    expense: create fetch all

--- app/Domain/Enums/ExpenseCategoryCodes.php
+++ app/Domain/Enums/ExpenseCategoryCodes.php
@@ -3,7 +3,8 @@ namespace App\Domain\Enums;
 
 enum ExpenseCategoryCodes: string
 {
-    case SUCCESS = 'EXPENSE_CATEGORY_CREATED';
+    case SUCCESS = 'EXPENSE_CATEGORY_SUCCESS';
+    case CREATED = 'EXPENSE_CATEGORY_CREATED';
     case FAILED = "EXPENSE_CATEGORY_FAILED";
     case MISSING = 'EXPENSE_CATEGORY_IS_MISSING';
 }
